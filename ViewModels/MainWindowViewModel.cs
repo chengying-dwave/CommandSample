1950eb9b195171350abbdf72fda555e64d2cff08
Cheng-Ying Li <chengying.li@dwave.cc>
2024-07-03 15:26:39 +0800

    feat: add OpenThePodBayDoorsFellowRobotCommand

--- ViewModels/MainWindowViewModel.cs
+++ ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.Windows.Input;
 using ReactiveUI;
 
@@ -23,6 +24,12 @@ public class MainWindowViewModel : ViewModelBase
     public ICommand OpenThePodBayDoorsDirectCommand { get; }
 
     /// <summary>
+    /// This command will ask HAL to open the pod bay doors, but this time we
+    /// check that the command is issued by a fellow robot (really any non-null name)
+    /// </summary>
+    public ICommand OpenThePodBayDoorsFellowRobotCommand { get; }
+
+    /// <summary>
     ///  This collection will store what the computer said
     /// </summary>
     public ObservableCollection<string> ConversationLog { get; } = new ObservableCollection<string>();
@@ -31,6 +38,14 @@ public class MainWindowViewModel : ViewModelBase
     {
         // Init OpenThePodBayDoorsDirectCommand
         OpenThePodBayDoorsDirectCommand = ReactiveCommand.Create(OpenThePodBayDoors);
+
+        // The IObservable<bool> is needed to enable or disable the command depending on valid parameters
+        // The Observable listens to RobotName and will enable the Command if the name is not empty.
+        IObservable<bool> canExecuteFellowRobotCommand =
+            this.WhenAnyValue(vm => vm.RobotName, (name) => !string.IsNullOrEmpty(name));
+
+        OpenThePodBayDoorsFellowRobotCommand =
+            ReactiveCommand.Create<string?>(name => OpenThePodBayDoorsFellowRobot(name), canExecuteFellowRobotCommand);
     }
 
     // The method that will be executed when the command is invoked
@@ -38,6 +53,12 @@ public class MainWindowViewModel : ViewModelBase
     {
         ConversationLog.Clear();
         AddToConvo("I'm sorry, Dave, I'm afraid I can't do that.");
+    }
+
+    private void OpenThePodBayDoorsFellowRobot(string? robotName)
+    {
+        ConversationLog.Clear();
+        AddToConvo($"Hello {robotName}, the Pod Bay is open :-)");
     }
 
     // Just a helper to add content to ConversationLog
